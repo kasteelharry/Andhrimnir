2ca34e6e4b1f0177cdff7d74c7aa36f5927bfc18
Joris Kuiper <joriskuiper2@gmail.com>
2022-11-03 16:30:36 +0100

    Made to run in headless mode

--- Andhrimnir.py
+++ Andhrimnir.py
@@ -5,8 +5,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 from src.CreateExcelSheet import createExcelFile
 import credentials
 from src.CrawlPage import goThroughPage
-
-PAGES = 1
+from selenium.webdriver.common.by import By
+PAGES = 5
 
 # The users using eetlijst.nl
 USERS = []
@@ -36,38 +36,54 @@ def combineUsersAndValues(users, values):
     return combined
 
 
+# Set the options for the driver.
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-gpu')
+options.add_argument('--disable-setuid-sandbox')
+options.add_argument("--window-size=1920,1080")
+options.add_argument("--start-maximized")
+options.add_argument("--incognito")
 # If you want to open Chrome
-driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.command_executor.set_timeout(10)
+path = ChromeDriverManager().install()
+driver = webdriver.Chrome(executable_path=path, options=options)
+driver.command_executor.set_timeout(40)
 driver.get('http://eetlijst.nl/login.php')
+# Wait for the page to load.
+driver.implicitly_wait(10)
 
 username = driver.find_element(By.NAME, "login")
 password = driver.find_element(By.NAME, "pass")
 username.send_keys(credentials.username)
 password.send_keys(credentials.password)
-driver.find_element(By.NAME, "dologin").click()
-
-driver.find_element(By.CLASS_NAME, "cc-btn").click()
-
+# Clicking the cookie banner if it exists.
+try:
+    banner = driver.find_element(By.CLASS_NAME, "cc-btn")
+    driver.execute_script("arguments[0].click();", banner)
+except Exception:
+    pass
+# Send the login form to continue
+login = driver.find_element(By.NAME, "dologin").click()
+# Navigate to the history page.
 driver.find_element(By.XPATH, "//*[@class='r']//a[2]").click()
 
-# Go through page
-
+# Go through the first page
 result = goThroughPage(driver)
 updateValues(result[0])
 USERS = result[1]
 
+# Loop over the remaining pages
 for i in range(PAGES):
-
     # Next page
     driver.find_element(By.NAME, "Volgende 25 ->").click()
-
     # Crawl the page
     result = goThroughPage(driver)
     updateValues(result[0])
 
+# Prepare the final results
 combined = combineUsersAndValues(USERS, VALUES)
-
+# Stop the driver
 driver.quit()
-
+# Create the excel file.
 createExcelFile(combined)
